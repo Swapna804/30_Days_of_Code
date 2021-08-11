73b151bfbc1c3fdd0ca391f51a3b6e099de53a29
Swapna Devi <65804603+Swapna804@users.noreply.github.com>
2021-08-11 19:16:47 +0530

    added

---
+++ DAY_2/1.cpp
@@ -0,0 +1,21 @@
+//minimum number of flips 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int c1=0,c2=0;
+        for(int i=0;i<s.length();i++){
+            if(i&1 and s[i]=='0')c1++;
+            if(i%2==0 and s[i]=='1')c1++;
+            if(i&1 and s[i]=='1')c2++;
+            if(i%2==0 and s[i]=='0')c2++;
+        }
+        cout<<min(c1,c2)<<endl;
+    }
+    return 0;
+}
